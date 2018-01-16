cd6355b341178872a4c6648fba4b85234d694ef6
pando <ndjman7@gmail.com>
2018-01-16 14:08:18 +0900

    Pystargram Part3까지 진행

---
+++ pystargram/pystargram/photos/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Photo(models.Model):
+    image = models.ImageField()
+    filtered_image = models.ImageField()
+    content = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
