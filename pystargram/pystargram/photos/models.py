fbf219f7880151ba6a0a42b5f8c93eb2ac4bfdf1
pando <ndjman7@gmail.com>
2018-01-16 14:46:03 +0900

    Pystargram Part5

--- pystargram/pystargram/photos/models.py
+++ pystargram/pystargram/photos/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='uploads/%Y/%m/%d/orig')
-    filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered')
+    image = models.ImageField(upload_to='%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered')
     content = models.TextField(max_length=500, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- pystargram/pystargram/photos/views.py
+++ pystargram/pystargram/photos/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Photo
+
+def detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    messages = (
+        '<p>{pk}번 사진</p>'.format(pk=photo.pk),
+        '<p>주소: {url}</p>'.format(url=photo.image.url),
+        '<p><img src="{url}" /></p>'.format(url=photo.image.url),
+    )
+    return HttpResponse(messages)
